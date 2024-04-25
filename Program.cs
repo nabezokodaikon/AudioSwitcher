8f0f3f7280ea2adc6c5dcbb33a989fa87e76cf2d
nabezokodaikon <nabezoko.daikon@gmail.com>
2024-04-26 06:12:56 +0900

    First commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AudioSwitcher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
